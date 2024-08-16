ab0844b13f48170c55499f0dfbc91f93d0c38c95
Dilimulati Diliyaer <dilyarswe@gmail.com>
2024-08-16 09:47:04 -0400

    some adjustments on chicken_roam state

--- scripts/pets/chicken.gd
+++ scripts/pets/chicken.gd
@@ -1,36 +1,14 @@
 extends CharacterBody2D
 ## Chicken pet class.
 ##
-## This class represents a chicken pet character in the game. The chicken can move 
-## and play different animations based on its velocity. It also updates the 
+## This class represents a chicken pet character in the game. It updates the 
 ## clickable area dynamically, allowing mouse events to interact with the chicken.
 
 @export var collision_polygon_2d: CollisionPolygon2D
-@export var visual: AnimatedSprite2D
-
-var idle_animations := ["chicken_idle", "chicken_idle_blink"]
 
 
-## Called every physics frame to update the chicken's movement and animations.
+## Called every physics frame to update the chicken's movement.
 func _physics_process(_delta):
-	# Play walking animation if the chicken is moving
-	if abs(velocity.x) > 0:
-		visual.play("chicken_walk")
-	else:
-		# Avoid replaying the same idle animation if it's already playing
-		if (visual.is_playing() and visual.animation in idle_animations):
-			return
-		
-		# Randomly choose an idle animation if the chicken is not moving
-		if (idle_animations.is_empty()):
-			visual.play("chicken_idle")
-		visual.play(idle_animations[randi() % idle_animations.size()])
-		
-		print(visual.animation)
-	
-	# Flip the sprite based on the direction of movement
-	visual.flip_h = velocity.x < 0
-	
 	# Move the chicken and handle collisions
 	move_and_slide()
 	
--- scripts/states/pets/chicken_roam.gd
+++ scripts/states/pets/chicken_roam.gd
@@ -1,13 +1,14 @@
 class_name ChickenRoam
 extends State
-## This state controls the chicken's roaming behavior within the screen bounds.
+## This state manages the chicken's roaming behavior within the screen boundaries.
 ##
-## The ChickenRoam state is part of the chicken's state machine. In this state,
-## the chicken will move in a random direction (-1, 0, 1) at a set speed for a
-## random amount of time. If the chicken reaches the screen boundaries, the
-## direction will change to ensure it stays within the screen.
+## As part of the chicken's state machine, the ChickenRoam state allows the chicken
+## to move in a random direction (-1, 0, or 1) at a constant speed for a randomly
+## determined duration. If the chicken reaches the edge of the screen, its direction
+## will change to keep it within the screen limits.
 
 @export var chicken: CharacterBody2D
+@export var visual: AnimatedSprite2D
 @export var move_speed := 40.0
 
 var move_direction: int
@@ -15,6 +16,8 @@ var roam_time: float
 
 var screen_left_bound: float = 25.0
 var screen_right_bound: float
+
+var idle_animations := ["chicken_idle", "chicken_idle_blink"]
 
 
 ## Randomizes the direction and duration for roaming within this state.
@@ -33,13 +36,14 @@ func enter() -> void:
 
 ## Updates the chicken's position and handles boundary detection while in this state.
 func update(delta: float) -> void:
-	# Check if the chicken hits the screen boundaries and adjust direction
-	if chicken.position.x <= screen_left_bound:
-		chicken.position.x = screen_left_bound
-		move_direction = 1
-	elif chicken.position.x >= screen_right_bound:
-		chicken.position.x = screen_right_bound
-		move_direction = -1
+	if chicken:
+		# Check if the chicken hits the screen boundaries and adjust direction
+		if chicken.position.x <= screen_left_bound:
+			chicken.position.x = screen_left_bound
+			move_direction = 1
+		elif chicken.position.x >= screen_right_bound:
+			chicken.position.x = screen_right_bound
+			move_direction = -1
 	
 	# Update the roaming time or randomize roaming if the time is up
 	if roam_time > 0:
@@ -48,7 +52,27 @@ func update(delta: float) -> void:
 		randomize_roaming()
 
 
-## Applies the movement based on the current direction and speed within this state.
+## Applies the movement based on the current direction and speed within this state,
+## also flip the sprite and play the animation according to actor's movement.
 func physics_update(_delta: float) -> void:
 	if chicken:
 		chicken.velocity.x = move_direction * move_speed
+		
+		# Flip the sprite based on the direction of movement
+		if chicken.velocity.x > 0:
+			visual.flip_h = false
+		elif chicken.velocity.x < 0:
+			visual.flip_h = true
+		
+		# Play walking animation if the chicken is moving
+		if abs(chicken.velocity.x) > 0:
+			visual.play("chicken_walk")
+		else:
+			# Avoid replaying the same idle animation if it's already playing
+			if (visual.is_playing() and visual.animation in idle_animations):
+				pass
+			else:
+				# Randomly choose an idle animation if the chicken is not moving
+				if (idle_animations.is_empty()):
+					visual.play("chicken_idle")
+				visual.play(idle_animations[randi() % idle_animations.size()])
